acfc662b2e1aa67e73db9972aaf5b54df051b95d
itsourov <sourovbuzz@gmail.com>
2023-02-10 00:39:15 +0600

    pagination works

--- app/Http/Livewire/Products/Index.php
+++ app/Http/Livewire/Products/Index.php
@@ -13,28 +13,33 @@ class Index extends Component
     use WithPagination;
 
     public $categories;
-    public $products;
+
     public $cat_id = [];
 
-    public $limitPerPage = 6;
-    public $hasMorePages;
+
 
     protected $queryString = ['cat_id' => ['compact' => ',']];
 
+
+
+
+    public function updated()
+    {
+        $this->resetPage();
+    }
+
+
     public function render()
     {
-        $this->products = Product::Filter(['categories' => $this->cat_id])->with('media')->take($this->limitPerPage)->get();
+
 
         $this->categories = cache()->remember('product_categories', 60, function () {
             return Category::where('type', 'productCategory')->withCount('products')->get();
         });
 
 
-        return view('livewire.products.index');
-    }
-
-    public function loadMore()
-    {
-        $this->limitPerPage = $this->limitPerPage + 6;
+        return view('livewire.products.index', [
+            'products' => Product::Filter(['categories' => $this->cat_id])->with('media')->paginate(6),
+        ]);
     }
 }
